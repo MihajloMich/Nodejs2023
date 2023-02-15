9a7f7294e8b8ea03c7c9473e1612d2dfd8766c69
MihajloMich <mihajloodimov03@gmail.com>
2023-02-15 21:06:36 +0100

    commit c2

---
+++ c2/index.js
@@ -0,0 +1,35 @@
+// console.log("zdravo svetu");
+// console.log(5 != 5);
+// console.log(typeof 25);
+
+// const chovek = {
+//   name: "John",
+//   surname: "Doe",
+// };
+
+// console.log(chovek);
+// a = 10;
+// console.log(a);
+// a = 12;
+// console.log(a);
+// const b = 15;
+// console.log(b);
+// b = 30
+//console.log(b);
+
+//let niza = [1, true, 2.5, "zdravo"];
+//console.log(niza);
+
+function testFn() {
+  return "Test fn initiated";
+}
+
+let test_fn = testFn();
+console.log(testFn());
+//testFn();
+
+const testFn1 = () => {
+  console.log("test fn1 initiated,this is arrow functionl");
+};
+
+testFn1();
